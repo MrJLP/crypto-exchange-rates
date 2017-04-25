4ff405414e601d9afa17e72f2810350e16e0f5c0
MrJLP <MrJLP@users.noreply.github.com>
2017-04-25 22:48:43 +0000

    use debug logging

--- test2.node.js
+++ test2.node.js
@@ -2,11 +2,16 @@
 
 'use strict'
 
-const currencyPairs = [ { source: 'BTC', dest: 'USD' }, { source: 'ETH', dest: 'USD' } ]
-
-const pluginNames = [ 'coinbase', 'bitstamp', 'bitcoinaverage', 'bravenewcoin' ]
+var debug = require('debug')
+var log = debug('test')
 
 var exchangeRateSources = require("./lib2/exchangeRateSources")
+
+const pluginNames = [ 'coinbase', 'bitstamp', 'bitcoinaverage', 'bravenewcoin' ]
+const currencyPairs = [ { source: 'BTC', dest: 'USD' }, { source: 'ETH', dest: 'USD' } ]
+
+log("pluginNames: %o", pluginNames)
+log("currencyPairs: %o", currencyPairs)
 
 var totalResults = {}
 pluginNames.forEach( function(name, index, array) {
@@ -14,11 +19,11 @@ pluginNames.forEach( function(name, index, array) {
   var plugin = exchangeRateSources[name]
 
   var name = plugin.getName()
-  console.log("\ngetName() = ", name)
+  log("getName() = ", name)
 
-  console.log("getCurrencyPairs(" + currencyPairs + ")...")
+  log("getCurrencyPairs(%o)...", currencyPairs)
   var ret = plugin.getCurrencyPairs( currencyPairs, function(results) {
-    console.log("\n", name + ": " + results)
+    log("RESULTS: %s: %o", name, results)
     totalResults[name] = results
   })
 })
